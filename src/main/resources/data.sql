3560f6469ce3bf211b2f004bb4fac18700ce253f
may9noy <may9noy@gmail.com>
2021-06-19 23:09:57 +0900

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CTALOG-001', 'Berlin', 100, 1500);
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CTALOG-002', 'Tokyo', 110, 1000);
+insert into catalog(product_id, product_name, stock, unit_price)
+    values('CTALOG-003', 'Stockholm', 120, 2000);
